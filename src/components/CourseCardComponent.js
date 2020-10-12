a719303ee5e35da39275b3e890e2178e46b8fe52
Colin Laakso <laxattack99@live.com>
2020-10-12 13:10:44 -0400

    implemented certain number of columns for certain screen sizes

--- src/components/CourseCardComponent.js
+++ src/components/CourseCardComponent.js
@@ -14,13 +14,13 @@ class CourseCardComponent extends React.Component {
   }
 
   updateTitle = (event) => {
-    const newTitle = event.target.value
-    const course = { ...this.state.course }
-    course.title = newTitle
+    const newTitle = event.target.value;
+    const course = { ...this.state.course };
+    course.title = newTitle;
     this.setState({
-      course: course
-    })
-  }
+      course: course,
+    });
+  };
 
   updateCourse = () => {
     this.setState({ editing: false });
@@ -29,43 +29,51 @@ class CourseCardComponent extends React.Component {
 
   render() {
     return (
-      <div className="card" styles={{ width: "18rem" }}>
-        <img className="card-img-top" src="https://picsum.photos/300/200" />
-        <div className="card-body">
-          <h5 className="card-title">
-          {
-            this.state.editing === true &&
-            <input
-              onChange={this.updateTitle}
-              value={this.state.course.title}/>
-          }
-          {
-            this.state.editing === false &&
-            <label>{this.state.course.title}</label>
-          }
-          </h5>
-          <p className="card-text">Card text.</p>
-          <Link className="btn btn-primary" to={`/course/edit/${this.state.course._id}`}>
-            More...
-          </Link>
-          <button
-            className="btn btn-danger"
-            onClick={() => this.props.deleteCourse(this.props.course)}
-          >
-            Delete
-          </button>
-          {
-            this.state.editing &&
-            <button onClick={() => this.updateCourse()} className="btn btn-primary">
-              Ok
+      <div className="spacing col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        <div className="card" styles={{ width: "18rem" }}>
+          <img className="card-img-top" src="https://picsum.photos/300/200" />
+          <div className="card-body">
+            <h5 className="card-title">
+              {this.state.editing === true && (
+                <input
+                  onChange={this.updateTitle}
+                  value={this.state.course.title}
+                />
+              )}
+              {this.state.editing === false && (
+                <label>{this.state.course.title}</label>
+              )}
+            </h5>
+            <p className="card-text">Card text.</p>
+            <Link
+              className="btn btn-primary"
+              to={`/course/edit/${this.state.course._id}`}
+            >
+              More...
+            </Link>
+            <button
+              className="btn btn-danger"
+              onClick={() => this.props.deleteCourse(this.props.course)}
+            >
+              Delete
             </button>
-          }
-          {
-            !this.state.editing &&
-            <button onClick={() => this.setState({editing: true})} className="btn btn-primary">
-              Edit
-            </button>
-          }
+            {this.state.editing && (
+              <button
+                onClick={() => this.updateCourse()}
+                className="btn btn-primary"
+              >
+                Ok
+              </button>
+            )}
+            {!this.state.editing && (
+              <button
+                onClick={() => this.setState({ editing: true })}
+                className="btn btn-primary"
+              >
+                Edit
+              </button>
+            )}
+          </div>
         </div>
       </div>
     );
